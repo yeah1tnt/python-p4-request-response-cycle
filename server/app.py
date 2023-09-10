8e6ad6327781dbb53ac789050d08076ceeed85e4
Tong Tang <pichip22@hotmail.com>
2023-09-10 11:36:04 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
+import os
 
-from flask import Flask
+from flask import Flask, request, current_app, g, make_response, redirect
 
 app = Flask(__name__)
+
+@app.before_request
+def app_path():
+    g.path=os.path.abspath(os.getcwd())
+
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name
+    response_body = f'''
+    <h1>The host for this page is {host}</h1>
+    <h2>The name of this application is {appname}</h2>
+    <h3>The path of this application on the user's device is {g.path}</h3>
+    '''
+
+    status_code = 200
+    headers={}
+    return make_response(response_body, status_code, headers)
+
+@app.route('/reginald-kenneth-dwight')
+def reginald_kenneth_dwight():
+    return redirect('names.com/elton-john')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
